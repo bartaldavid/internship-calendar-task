cdf0fd73bcf27ec85e4aaa6a75e57b22a96806a9
Marc <7101849+marcnajera@users.noreply.github.com>
2023-10-14 01:30:39 +0200

    Initial commit

---
+++ src/lib/Calendar.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+  import { DateTime } from "luxon";
+  import { createDialog } from '@melt-ui/svelte'
+  import { Appointments } from "./events";
+</script>
+
+<!-- YOUR WORK STARTS HERE -->
+<div class="bg-white rounded-lg shadow-lg w-full h-full flex items-center justify-center">
+  <p class="text-5xl text-center">Turn this into a calendar</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Calendar from "$lib/Calendar.svelte"
+</script>
+
+<div class="w-screen h-screen bg-slate-500 p-10 flex items-center justify-center">
+  <Calendar />
+</div>
